4498d5e3db906b83a9c08036a4462c72cd40cb79
Hal Wine <hwine@mozilla.com>
2018-02-14 11:57:56 -0800

    Improved debug support
    Based on 2nd use of package, output is via python logging module. Added
    support for "headless" operation.

--- github_selenium/__init__.py
+++ github_selenium/__init__.py
@@ -7,33 +7,33 @@ __email__ = 'hwine@mozilla.com'
 __version__ = '0.1.0'
 
 
+import logging
 import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import (
+from selenium.common.exceptions import (  # noqa: F401,W0611
     NoSuchElementException,
     TimeoutException,
     WebDriverException,
 )
 
-WebDriverException
+logger = logging.getLogger(__name__)
 
 
 class GitHub2FA(object):
 
-    def __init__(self, implicit_wait=5):
-        # force headless mode
-        # TODO make option
-        os.environ['MOZ_HEADLESS'] = "1"
+    def __init__(self, implicit_wait=5, headless=True):
+        # set headless mode
+        if headless:
+            os.environ['MOZ_HEADLESS'] = "1"
         driver = webdriver.Firefox()
         driver.wait = WebDriverWait(driver, implicit_wait)
         self.driver = driver
 
     def get_element(self, name_or_id):
         element = None
-        e = None
         for method in By.NAME, By.ID, By.CSS_SELECTOR:
             try:
                 element = self.driver.find_element(method, name_or_id)
@@ -41,7 +41,7 @@ class GitHub2FA(object):
             except NoSuchElementException:
                 pass
             except Exception as e:
-                print("got {} for {}".format(repr(e), name_or_id))
+                logger.debug("got {} for {}".format(repr(e), name_or_id))
         return element
 
     def fill_field(self, name_or_id, text_to_send, submit=False):
@@ -57,22 +57,33 @@ class GitHub2FA(object):
     def wait_for_page(self, title_text):
         try:
             self.driver.wait.until(EC.title_contains(title_text))
+            logger.debug("Got to: {}".format(title_text))
         except TimeoutException:
+            logger.debug("Did NOT get to: {}".format(title_text))
             raise ValueError("Didn't arrive on {} page"
                              .format(title_text.encode('UTF-8')))
 
     def login(self, login, password, destination_page_url,
               destination_page_title, token):
+        self.driver.get("https://github.com/login")
+        try:
+            self.wait_for_page("Sign in")
+            self.fill_field('login', login)
+            self.fill_field('password', password)
+            self.click_button('commit')
+            # put in token
+            self.wait_for_page("Where software is built")
+            self.fill_field('otp', token, submit=True)
+            # we have to wait for the page to refresh, the last thing that
+            # seems to be updated is the title
+            self.wait_for_page("Github")
+        except ValueError as e:
+            if "GitHub" not in self.driver.title:
+                # didn't get to post login page
+                logger.debug("Didn't get to {}".format(destination_page_url))
+                logger.debug("latest title '{}'".format(self.driver.title))
+                raise e
         self.driver.get(destination_page_url)
-        self.wait_for_page("Sign in")
-        self.fill_field('login', login)
-        self.fill_field('password', password)
-        self.click_button('commit')
-        # put in token
-        self.wait_for_page("Where software is built")
-        self.fill_field('otp', token, submit=True)
-        # we have to wait for the page to refresh, the last thing that
-        # seems to be updated is the title
         self.wait_for_page(destination_page_title)
 
     def quit(self):
